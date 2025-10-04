d366eac88b3d3a555e977b2057561236b314c888
QuangVMSE192755 <vminhquang05@gmail.com>
2025-10-04 16:47:29 +0700

    Fix dang nhap

--- gamingShop/src/java/controller/MainController.java
+++ gamingShop/src/java/controller/MainController.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MainController",
-        urlPatterns = {"/product/*", "/accessory/*", "/service/*", "/post/*", "/MainController", "/mc", ""}
+        urlPatterns = {"/admin/*", "/product/*", "/accessory/*", "/service/*", "/post/*", "/MainController", "/mc", ""}
 )
 @MultipartConfig(
         fileSizeThreshold = 1024 * 1024 * 2, // 2MB
